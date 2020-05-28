ce77b614aaa6c5f0a17bb6ddc3b15c4c31e47bd7
Tim Abrahamson <tim@kinetic.com>
2020-05-28 15:34:47 -0500

    Added a some scroll and cursor animations

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let controller;
 let slideScene;
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 
 function animateSlides() {
   //Init controller
@@ -8,7 +11,7 @@ function animateSlides() {
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
   //Loop over each slide
-  sliders.forEach(slide => {
+  sliders.forEach((slide, index, slides) => {
     const revealImage = slide.querySelector(".reveal-image");
     const image = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -20,7 +23,81 @@ function animateSlides() {
     slideTimeline.fromTo(image, { scale: 2 }, { scale: 1 }, "-=1");
     slideTimeline.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
     slideTimeline.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+    //Create scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false
+    })
+      .setTween(slideTimeline)
+      .addIndicators()
+      .addTo(controller);
+    //New animation
+    const pageTimeline = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTimeline.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageTimeline.fromTo(
+      slide,
+      { opacity: 1, scale: 1 },
+      { opacity: 0, scale: 0.5 }
+    );
+    pageTimeline.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    //Create new scene
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0
+    })
+      .addIndicators()
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTimeline)
+      .addTo(controller);
   });
 }
+
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseText.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    gsap.to(".title-swipe", 1, { y: "100%" });
+    mouseText.innerText = "";
+  }
+}
+
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "#000" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "#000" });
+    gsap.to("#logo", 1, { color: "#000" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "#fff" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "#fff" });
+    gsap.to("#logo", 1, { color: "#fff" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+burger.addEventListener("click", navToggle);
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 
 animateSlides();
