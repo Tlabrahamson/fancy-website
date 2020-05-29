ead4f94a5200d0482fe035070bafb0d1aca78c59
Tim Abrahamson <tim@kinetic.com>
2020-05-29 14:47:57 -0500

    Finished JS. Working on responsive design.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let detailScene;
 const mouse = document.querySelector(".cursor");
 const mouseText = mouse.querySelector("span");
 const burger = document.querySelector(".burger");
@@ -23,7 +24,6 @@ function animateSlides() {
     slideTimeline.fromTo(revealImage, { x: "0%" }, { x: "100%" });
     slideTimeline.fromTo(image, { scale: 2 }, { scale: 1 }, "-=1");
     slideTimeline.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideTimeline.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
     //Create scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -114,12 +114,11 @@ barba.init({
       namespace: "fashion",
       beforeEnter() {
         logo.href = "../index.html";
-        gsap.fromTo(
-          ".nav-header",
-          1,
-          { y: "100%" },
-          { y: "0%", ease: "power2.inOut" }
-        );
+        detailAnimation();
+      },
+      beforeLeave() {
+        controller.destroy();
+        detailScene.destroy();
       }
     }
   ],
@@ -157,10 +156,39 @@ barba.init({
           { x: "100%", stagger: 0.25, onComplete: done }
         );
         timeline.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        timeline.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        );
       }
     }
   ]
 });
+
+function detailAnimation() {
+  controller = new ScrollMagic.Controller();
+  const slides = document.querySelectorAll(".detail-slide");
+  slides.forEach((slide, index, slides) => {
+    const slideTimeline = gsap.timeline({ defaults: { duration: 1 } });
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    const nextImage = nextSlide.querySelector("img");
+    slideTimeline.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTimeline.fromTo(nextSlide, { opactiy: 0 }, { opacity: 1 }, "-=1");
+    slideTimeline.fromTo(nextImage, { x: "200%" }, { x: "0%" });
+    //Scene
+    detailScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTimeline)
+      .addTo(controller);
+  });
+}
 
 //Event Listeners
 burger.addEventListener("click", navToggle);
